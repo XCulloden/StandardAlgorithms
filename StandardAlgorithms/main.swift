bf37644ae962c611d02db2b10270ce867b538ce2
Xan Culloden <Culloden.x@etoncollege.org.uk>
2023-11-07 11:02:44 +0000

    adding constant b

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -4,8 +4,10 @@
 //
 //  Created by Culloden, Xan (EJNR) on 07/11/2023.
 //
-
 import Foundation
 
-print("Hello, World!")
+var a = 5
+a += 7
 
+let b = "hi everyone ImLosiNgs My VOicE"
+print("a is now: \(a)")
