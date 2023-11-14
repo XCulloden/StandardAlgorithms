84bbdd97a1db6a4f7ab5076510cab5219cfd3fd3
XCulloden <Culloden.x@etoncollege.org.uk>
2023-11-14 10:16:25 +0000

    merge sort and tests added

--- StandardAlgorithmTests/SortingTests.swift
+++ StandardAlgorithmTests/SortingTests.swift
@@ -40,18 +40,60 @@ final class SortingTests: XCTestCase {
         
     }
     
-    func TestBubbleSortWithManyShuffledArraysOfItsAndReturnsInAcsemdingOrder(){
+//    func TestBubbleSortWithManyShuffledArraysOfItsAndReturnsInAcsemdingOrder(){
+//        //arrange
+//        let sorting = Sorting()
+//        
+//        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
+//                        (data: [6,1,2,7,4],expected: [1,2,4,6,7]),
+//                        (data: [5,1,9,7,4],expected: [1,4,5,7,9])]
+//        //act
+//        for testCase in textCases {
+//            let actual = sorting.bubbleSort(testCase.data)
+//            XCTAssertEqual(actual, testCase.expected)
+//        }
+//        //assert
+//    }
+    func testMergeSortWithShuffledInt(){
         //arrange
         let sorting = Sorting()
-        
-        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
-                        (data: [6,1,2,7,4],expected: [1,2,4,6,7]),
-                        (data: [5,1,9,7,4],expected: [1,4,5,7,9])]
+        let data = [5,1,3,7,4]
+        let expected = [1,3,4,5,7]
         //act
-        for testCase in textCases {
-            let actual = sorting.bubbleSort(testCase.data)
-            XCTAssertEqual(actual, testCase.expected)
-        }
+        let actual = sorting.MergeSort(data)
         //assert
+        XCTAssertEqual(actual,expected)
+    }
+    func testMergeSortWithOtherShuffledInt(){
+        //arrange
+        let sorting = Sorting()
+        let data = [9,4,8,3,2,5,1,0,7]
+        let expected = [0,1,2,3,4,5,7,8,9]
+        //act
+        let actual = sorting.MergeSort(data)
+        //assert
+        XCTAssertEqual(actual,expected)
+    }
+    
+    func testMergeSortWithReverseSortedInt(){
+        //arrange
+        let sorting = Sorting()
+        let data = [10,9,8,7,6,5,4,3,2,1,0]
+        let expected = [0,1,2,3,4,5,6,7,8,9,10]
+        //act
+        let actual = sorting.MergeSort(data)
+        //assert
+        XCTAssertEqual(actual,expected)
+    }
+    
+    func testMergeSortWithRepeatingInt(){
+        //arrange
+        let sorting = Sorting()
+        let data = [7,7,4,5,4,1]
+        let expected = [1,4,4,5,7,7]
+        //act
+        let actual = sorting.MergeSort(data)
+        //assert
+        XCTAssertEqual(actual,expected)
     }
 }
--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -14,4 +14,39 @@ class Sorting{
         }
         return [1,3,4,5,7]
     }
+    
+    func MergeSort(_ array: [Int]) -> [Int] {
+        guard array.count > 1 else{
+            return array
+        }
+        
+        let mid = array.count / 2
+        let firstArray = MergeSort(Array(array[0..<mid]))
+        let secondArray = MergeSort(Array(array[mid..<array.count]))
+        
+        return sortArrays(firstArray, secondArray)
+    }
+    
+    func sortArrays(_ firstArray: [Int], _ secondArray: [Int]) -> [Int]{
+        var sortedArray = [Int]()
+        var FirstIndex = 0
+        var SecondIndex = 0
+        
+        
+        while  FirstIndex < firstArray.count && SecondIndex < secondArray.count{
+            if firstArray[FirstIndex] < secondArray[SecondIndex]{
+                sortedArray.append(firstArray[FirstIndex])
+                FirstIndex += 1
+            }
+            else if secondArray[SecondIndex] < firstArray[FirstIndex]{
+                sortedArray.append(secondArray[SecondIndex])
+                SecondIndex += 1
+            }
+        }
+        sortedArray.append(contentsOf: firstArray[FirstIndex...])
+        sortedArray.append(contentsOf: secondArray[SecondIndex...])
+        
+        return sortedArray
+    }
 }
+
